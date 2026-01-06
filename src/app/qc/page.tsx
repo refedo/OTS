b59c1dd68dd0980f97c59f44bca5f0e2c6d84479
refedo <refedo@yahoo.com>
2026-01-06 23:48:46 +0300

    Fix QC dashboard width issue - use lg:pl-64 layout pattern

--- src/app/qc/page.tsx
+++ src/app/qc/page.tsx
@@ -162,7 +162,7 @@ export default function QCDashboardPage() {
   };
 
   return (
-    <div className="space-y-6">
+    <div className="p-6 space-y-6">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div>
